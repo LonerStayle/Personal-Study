8efdd3d792e0a6e0dfda778975d762fd1fa694e1
LonerStayle <dlwlstjq410@gmail.com>
2023-07-10 17:44:06 +0900

    Coroutine - 이터레이터 예제 추가

--- app/src/main/java/kr/loner/personalstudy/coroutine/Coroutine.kt
+++ app/src/main/java/kr/loner/personalstudy/coroutine/Coroutine.kt
@@ -99,3 +99,25 @@ suspend fun channelTest(){
 fun CoroutineScope.channelProduceTest(): ReceiveChannel<Int> = produce {
     repeat(5){ send(it)}
 }
+
+
+fun getNumberIteratorFrom(start: Int) = iterator {
+    var x = start
+    while (true) yield(x++) // infinite stream of integers from start
+}
+
+fun getFilteredNumberIterator(numbers: Iterator<Int>, prime: Int) = iterator {
+    for (x in numbers) {
+        if (x % prime != 0) yield(x)
+    }
+}
+
+fun pipeLineIterator(){
+    var cur = getNumberIteratorFrom(2)
+    for (i in 1..10) {
+        val prime = cur.next()
+        println(prime)
+        cur = getFilteredNumberIterator(cur, prime)
+    }
+    println("Done")
+}
